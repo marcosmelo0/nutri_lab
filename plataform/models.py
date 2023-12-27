cc4ab2b2ef103c8f3915a4b50d0e4e4632236daf
Marcos Melo <marcosmelo012@gmail.com>
2023-12-27 18:53:37 -0300

    feat: add .gitignore and create plataform migration

---
+++ plataform/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Patients(models.Model):
+    choices_sex = (('F', 'Feminino'), ('M', 'Maculino'))
+    name = models.CharField(max_length=50)
+    sex = models.CharField(max_length=1, choices=choices_sex)
+    age = models.IntegerField()
+    email = models.EmailField()
+    telephone = models.CharField(max_length=19)
+    nutri = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
